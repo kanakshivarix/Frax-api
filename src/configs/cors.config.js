0119a85e18c1b8ba30cbdae24fd9664ba3bf58f2
MrYogesh0709 <yogeshvanzara98@gmail.com>
2026-03-30 10:39:19 +0530

    cors urls changed

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://subcheliform-tentier-lanie.ngrok-free.dev",
-  "https://commercially-untrumping-kathrin.ngrok-free.dev",
-  "https://commercially-untrumping-kathrin.ngrok-free.dev",
-  "https://commercially-untrumping-kathrin.ngrok-free.dev",
-  "http://127.0.0.1:5173",
-  "http://127.0.0.1:5174",
-  
-
+  "https://cafewafe.com",
+  "https://admin.cafewafe.com",
 ];
-=======
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:5174", "http://localhost:5173/" ,"https://cafewafe.com/" , "https://admin.cafewafe.com/"];
->>>>>>> f6303c56691410e5f700b20796d5aa9beae89b69
 
 exports.corsOptions = {
   origin: (origin, callback) => {
